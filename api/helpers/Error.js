159f36fbd3eb75180f0274f459acb0e39fde52ad
jen_zim <jzimmerman@shoppertrak.com>
2018-03-28 12:48:43 -0500

    added recipe deletion, improved error helper

--- api/helpers/Error.js
+++ api/helpers/Error.js
@@ -1,13 +1,26 @@
 module.exports = {
-  buildErrorResponse(err) {
-    const returnError = err;
+  friendlyName: 'buildErrorResponse',
+
+  description: 'standardizes error response returned to the UI',
+
+  inputs: {
+    err: {
+      type: 'ref',
+      example: new Error(),
+      description: 'the error thrown by the API/db',
+      required: true
+    }
+  },
+
+  async fn (inputs, exits) {
+    const returnError = inputs.err;
     if (!returnError.message) {
       if (returnError.errmsg) {
         returnError.message = returnError.errmsg;
       } else {
         returnError.message = 'Encountered an error when submitting recipe form';
       }
-      return returnError;
     }
+    return exits.success(returnError);
   }
 };
